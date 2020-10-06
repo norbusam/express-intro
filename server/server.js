c8f88c10d29713f1db53ad957e550c7dd7c2d5e1
Your Name <norbusamphel@Norbus-MacBook-Pro.local>
2020-10-06 17:03:17 -0500

    modules created

--- server/server.js
+++ server/server.js
@@ -5,8 +5,27 @@ const app = express();
 // a port where the server is going to run(like an address/url)
 const port = 5000;
 
+const quotesData = require('./modules/quotes.js')
 // express static file serving - public is the folder name
 app.use(express.static('server/public'));
+
+
+
+let index = 0;
+//get route
+app.get('/quotes', (req, res) => {
+    console.log('hi from get request');
+    res.send(quotesData);
+})
+
+app.get('/randomQuote', (req, res) => {
+    let randomNumber = getRandom(quotesData.length)
+    res.send(quotesData[randomNumber])
+})
+
+function getRandom (max){
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
 // () => {} is an anon function
 app.listen(port, () => {
